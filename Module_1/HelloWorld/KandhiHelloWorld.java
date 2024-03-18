628349d3cb5cd2151b9f8678de1336d6b4161670
skandhibellevue <skandhi@my365.bellevue.edu>
2024-03-17 23:51:57 -0500

    Create KandhiHelloWorld.java
    Added hello world class and Module 1 folder structure

---
+++ Module_1/HelloWorld/KandhiHelloWorld.java
@@ -0,0 +1,19 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by Swetha Kandhi 2024
+*/ 
+import java.util.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        // Define first and last name variables
+        String firstName = "Swetha";
+        String lastName = "Kandhi";
+    
+        // Output the string using printf
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    } //end main
+} // end HelloWorld
